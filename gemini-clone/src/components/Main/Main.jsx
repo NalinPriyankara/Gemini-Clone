fcff678623723518d4f1bfec9b2141399a892751
NalinPriyankara <112267206+NalinPriyankara@users.noreply.github.com>
2024-06-16 22:26:58 +0530

    update 2

--- gemini-clone/src/components/Main/Main.jsx
+++ gemini-clone/src/components/Main/Main.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './main.css';
+import { assets } from '../../assets/assets';
+import { Context } from '../../context/Context';
 
 const Main = () => {
+
+  const { onSent, recentPrompt, showResult, loading, resultData, setInput, input } = useContext(Context);
+
   return (
     <div className='main'>
       <div className="nav">
@@ -9,6 +14,8 @@ const Main = () => {
         <img src={assets.user_icon} alt="" />
       </div>
       <div className="main-container">
+        {!showResult?
+        <>
         <div className="greet">
             <p><span>Hello, Dev</span></p>
             <p>How can I help you today?</p>
@@ -31,13 +38,35 @@ const Main = () => {
                 <img src={assets.code_icon} alt="" />
             </div>
         </div>
+        </>
+        :
+        <div className="result">
+          <div className="result-title">
+            <img src={assets.user_icon} alt="" />
+            <p>{recentPrompt}</p>
+          </div>
+          <div className="result-data">
+            <img src={assets.gemini_icon} alt="" />
+            {loading?
+            <div className='loader'>
+              <hr />
+              <hr />
+              <hr />
+            </div>
+            : <p dangerouslySetInnerHTML={{__html:resultData}}></p>
+            }
+          </div>
+        </div>
+        }
+        
+        
         <div className="main-bottom">
             <div className="search-box">
-                <input type="text" placeholder='Enter a promt here' />
+                <input onChange={(e) => setInput(e.target.value)} type={input} placeholder='Enter a promt here' />
                 <div>
                     <img src={assets.gallery_icon} alt="" />
                     <img src={assets.mic_icon} alt="" />
-                    <img src={assets.send_icon} alt="" />
+                    {input? <img onClick={() => onSent()} src={assets.send_icon} alt="" /> : null}
                 </div>
             </div>
             <p className="bottom-info">
@@ -48,7 +77,5 @@ const Main = () => {
     </div>
   )
 }
-import './main.css';
-import { assets } from '../../assets/assets';
 
 export default Main
